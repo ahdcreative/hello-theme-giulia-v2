5c50694a5ea177bd84ab7ae3655ace96628dc9da
l.forzutti <l.forzutti@egeatech.com>
2022-07-28 17:01:30 +0200

    Update enqueue.php

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,9 +4,10 @@ class HelloEnqueue{
         add_action('wp_enqueue_scripts', array('HelloEnqueue', 'enqueue_styles'));
         //add_action('wp_enqueue_scripts', array('HelloEnqueue', 'enqueue_scripts'));
     }
+
     public static function enqueue_styles(){
         wp_enqueue_style('hello-elementor-child-style', get_stylesheet_directory_uri() . '/style.css', ['hello-elementor-theme-style',	],	'1.0.0'	);
-        wp_enqueue_style('hello-child-styles', get_stylesheet_directory() . '/assets/css/giulia-child.css', array(), false, 'all');
+        wp_enqueue_style('giulia-child-styles', get_stylesheet_directory() . '/assets/css/giulia-child.css', array(), false, 'all');
     }
 }
 HelloEnqueue::init();
