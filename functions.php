7486f1677c2cff8b3757dd54c5c9ff03e7d295d0
darka <luca@ahd-creative.com>
2022-07-29 14:40:58 +0200

    Fix CSS Not Loading

--- functions.php
+++ functions.php
@@ -1,35 +1,8 @@
 <?php
-define( '__CHILD_INC__', __DIR__ . '/inc/', true );
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+function hello_elementor_child_enqueue(): void
+{
+    wp_enqueue_style('hello-elementor-child-theme', get_stylesheet_directory_uri() . '/style.css', ['hello-elementor-theme-style'], '1.0', 'all');
+    wp_enqueue_style('ottica-giulia', get_stylesheet_directory_uri() . '/assets/css/giulia-child.css', ['hello-elementor-child-theme'], '1.0', 'all');
 }
-if ( ! function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ) {
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) ) {
-            $uri = get_template_directory_uri() . '/rtl.css';
-        }
 
-        return $uri;
-    }
-endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
-if ( ! function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array(
-            'hello-elementor',
-            'hello-elementor',
-            'hello-elementor-theme-style'
-        ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
-
-class LinkSide {
-    public static function init() {
-        ( new LinkSide )->load();
-    }
-
-    private function load() {
-        require __CHILD_INC__ . '__loader.php';
-    }
-}
+add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue', 30);
