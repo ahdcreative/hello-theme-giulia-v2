e6876ace17d5ef4d08aaedbc9b17dc5567347e70
darka <luca@ahd-creative.com>
2022-07-29 13:14:40 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,38 +1,35 @@
 <?php
-//CONSTANTS
-define('__CHILD_INC__', __DIR__ . '/inc/', true);
-
-
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
-
-// BEGIN ENQUEUE PARENT ACTION
-// AUTO GENERATED - Do not modify or remove comment markers above or below:
-
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+define( '__CHILD_INC__', __DIR__ . '/inc/', true );
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+if ( ! function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ) {
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) ) {
             $uri = get_template_directory_uri() . '/rtl.css';
+        }
+
         return $uri;
     }
 endif;
 add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
-
-if ( !function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'hello-elementor','hello-elementor','hello-elementor-theme-style' ) );
+if ( ! function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css(): void {
+        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array(
+            'hello-elementor',
+            'hello-elementor',
+            'hello-elementor-theme-style'
+        ) );
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
-// END ENQUEUE PARENT ACTION
-// START SITE CLASS
-class GiuliaSide{
-    public static function init(){
-        self::load('__loader');
+class LinkSide {
+    public static function init(): void {
+        ( new LinkSide )->load();
     }
-    private function load($file){
-        require __CHILD_INC__ . $file;
 
+    private function load(): void {
+        require __CHILD_INC__ . '__loader.php';
     }
 }
