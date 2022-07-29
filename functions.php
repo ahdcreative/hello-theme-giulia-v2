483690112421e115579483332c220cd75489850b
darka <luca@ahd-creative.com>
2022-07-29 16:06:42 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,12 @@
 function hello_elementor_child_enqueue(): void
 {
     wp_enqueue_style('hello-elementor-child-theme', get_stylesheet_directory_uri() . '/style.css', ['hello-elementor-theme-style'], '1.0', 'all');
-    wp_enqueue_style('ottica-giulia', get_stylesheet_directory_uri() . '/assets/css/giulia-child.css', ['hello-elementor-child-theme'], '1.0', 'all');
 }
 
+
+function giulia_css(): void
+{
+    wp_enqueue_style('ottica-giulia', get_stylesheet_directory_uri() . '/assets/css/giulia-child.css', ['hello-elementor-child-theme'], '1.0', 'all');
+}
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue', 30);
+add_action('wp_enqueue_scripts', 'giulia_css', 99);
