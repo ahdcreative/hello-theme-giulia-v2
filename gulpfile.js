9cbd6fef7421216ef985c3449acae9e999736b89
DarkArtek <darkartek@gmail.com>
2022-09-11 14:29:31 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass')(require('sass')),
-    cssmin = require('gulp-cssnano'),
-    sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer'),
-    prefixerOptions = {
-        overrideBrowserslist: ['last 2 versions']
-    }
+const gulp = require('gulp'),
+  sass = require('gulp-sass')(require('sass')),
+  cssmin = require('gulp-cssnano'),
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  prefixerOptions = {
+    overrideBrowserslist: ['last 2 versions'],
+  };
 
 sass.compiler = require('node-sass');
 
 gulp.task('giulia', function () {
-    return gulp.src('src/sass/giulia-child.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer(prefixerOptions))
-        .pipe(cssmin({zindex: false}))
-        .pipe(sourcemaps.write('.', undefined))
-        .pipe(gulp.dest('assets/css/'))
+  return gulp
+    .src('src/sass/giulia-child.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(prefixerOptions))
+    .pipe(cssmin({ zindex: false }))
+    .pipe(sourcemaps.write('.', undefined))
+    .pipe(gulp.dest('assets/css/'));
 });
